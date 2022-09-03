57eca1c930d7cc129d6311827b9c7085d9a45152
ZreXoc <zeexoc@outlook.com>
2022-09-03 11:29:40 +0800

    del

--- libs/util/src/lib/blog.ts
+++
@@ -1,9 +0,0 @@
-import { getMatterBySlug } from '@zrexoc/markdown';
-import fs from 'fs'
-export function getRecent(){
-    return   fs
-    .readdirSync(BLOGS_PATH)
-    .map((path) => path.replace(/\.mdx?$/, ''))
-    .map((slug) => getMatterBySlug(slug, BLOGS_PATH).data)
-    .sort((a, b) => (b.time > a.time ? 1 : -1));
-}
